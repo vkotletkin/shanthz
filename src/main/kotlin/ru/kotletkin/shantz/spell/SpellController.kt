363a8cedc5b721ff42b339751c4f330892b53e14
Vladislav Kotletkin <drzhkn1@mail.ru>
2026-03-09 22:22:37 +0300

    feat: added logger

--- src/main/kotlin/ru/kotletkin/shantz/spell/SpellController.kt
+++ src/main/kotlin/ru/kotletkin/shantz/spell/SpellController.kt
@@ -1,9 +1,13 @@
 package ru.kotletkin.shantz.spell
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import jakarta.validation.Valid
 import org.languagetool.rules.RuleMatch
 import org.springframework.web.bind.annotation.*
 import ru.kotletkin.shantz.spell.dto.SpellDTO
+
+
+private val logger = KotlinLogging.logger {}
 
 @RestController
 @RequestMapping("/api/spell")
@@ -11,11 +15,13 @@ class SpellController(private val spellService: SpellService) {
 
     @GetMapping
     fun getSpellingLanguages(): List<String> {
+        logger.info { "Запрошены поддерживаемые языки" }
         return spellService.getSpellingLanguages()
     }
 
     @PostMapping
     fun checkSpell(@Valid @RequestBody spellDTO: SpellDTO): List<RuleMatch> {
+        logger.info { "Запрошена валидация текста по языку: ${spellDTO.language}" }
         return spellService.checkSpellingOnLanguage(spellDTO)
     }
 }
